b2302f8fa00b8063d7d4eddc8456260a1e53d5c7
saitejabasudi <244159782+saitejabasudi@users.noreply.github.com>
2026-02-20 19:30:55 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,28 +2,41 @@ import React from "react";
 import { FaHome, FaFileAlt, FaStar, FaUser } from "react-icons/fa";
 
 function Navbar({ setPage, currentPage }) {
-  const iconStyle = (page) =>
-    `cursor-pointer ${currentPage === page ? "text-yellow-400" : ""}`;
+  const activeStyle = "text-yellow-400";
+  const normalStyle = "text-gray-400";
 
   return (
-    <div className="fixed bottom-4 left-0 right-0 flex justify-center">
-      <div className="bg-black text-white px-8 py-3 rounded-full flex gap-10 text-lg shadow-lg">
-        <FaHome
-          className={iconStyle("home")}
-          onClick={() => setPage("home")}
-        />
-        <FaFileAlt
-          className={iconStyle("history")}
-          onClick={() => setPage("history")}
-        />
-        <FaStar
-          className={iconStyle("tips")}
-          onClick={() => setPage("tips")}
-        />
-        <FaUser
-          className={iconStyle("profile")}
-          onClick={() => setPage("profile")}
-        />
+    <div className="fixed bottom-5 left-0 right-0 flex justify-center">
+      <div className="bg-black w-80 py-3 rounded-full flex justify-around items-center shadow-lg">
+
+        <button onClick={() => setPage("home")}>
+          <FaHome
+            size={20}
+            className={currentPage === "home" ? activeStyle : normalStyle}
+          />
+        </button>
+
+        <button onClick={() => setPage("history")}>
+          <FaFileAlt
+            size={20}
+            className={currentPage === "history" ? activeStyle : normalStyle}
+          />
+        </button>
+
+        <button onClick={() => setPage("tips")}>
+          <FaStar
+            size={20}
+            className={currentPage === "tips" ? activeStyle : normalStyle}
+          />
+        </button>
+
+        <button onClick={() => setPage("profile")}>
+          <FaUser
+            size={20}
+            className={currentPage === "profile" ? activeStyle : normalStyle}
+          />
+        </button>
+
       </div>
     </div>
   );
