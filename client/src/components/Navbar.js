671ddb0ab0edbbf72f619c4779161856a0173578
saitejabasudi <244159782+saitejabasudi@users.noreply.github.com>
2026-02-20 19:20:39 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { FaHome, FaFileAlt, FaStar, FaUser } from "react-icons/fa";
 
-function Navbar() {
+function Navbar({ setPage, currentPage }) {
+  const iconStyle = (page) =>
+    `cursor-pointer ${currentPage === page ? "text-yellow-400" : ""}`;
+
   return (
     <div className="fixed bottom-4 left-0 right-0 flex justify-center">
       <div className="bg-black text-white px-8 py-3 rounded-full flex gap-10 text-lg shadow-lg">
-        <FaHome />
-        <FaFileAlt />
-        <FaStar />
-        <FaUser />
+        <FaHome
+          className={iconStyle("home")}
+          onClick={() => setPage("home")}
+        />
+        <FaFileAlt
+          className={iconStyle("history")}
+          onClick={() => setPage("history")}
+        />
+        <FaStar
+          className={iconStyle("tips")}
+          onClick={() => setPage("tips")}
+        />
+        <FaUser
+          className={iconStyle("profile")}
+          onClick={() => setPage("profile")}
+        />
       </div>
     </div>
   );
