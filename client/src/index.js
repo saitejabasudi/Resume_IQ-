4cb1f3f93f9b2dccbed585a994d2023892574fc9
saitejabasudi <244159782+saitejabasudi@users.noreply.github.com>
2026-02-20 16:09:18 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,3 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("Service Worker Registered"));
-  });
-}
