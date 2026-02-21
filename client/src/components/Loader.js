078b2c3f2b6caeca9ef2018100a89b2413d618a3
saitejabasudi <244159782+saitejabasudi@users.noreply.github.com>
2026-02-21 17:29:14 +0530

    Update Loader.js

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="flex justify-center items-center py-6">
-      <div className="w-12 h-12 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin"></div>
+    <div className="flex flex-col justify-center items-center py-10 space-y-4">
+      
+      {/* Spinner */}
+      <div className="w-14 h-14 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin dark:border-yellow-500"></div>
+
+      {/* Loading Text */}
+      <p className="text-gray-600 dark:text-gray-300 font-medium animate-pulse">
+        Analyzing Resume...
+      </p>
+
     </div>
   );
 }
