bb0cd0ff6cdf39e0c2428ff57395d2bf9e5dd3d2
saitejabasudi <244159782+saitejabasudi@users.noreply.github.com>
2026-02-21 16:49:19 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,27 @@
 import React from "react";
-import { FaHome, FaFileAlt, FaStar, FaUser } from "react-icons/fa";
+import { FaHome, FaHistory, FaLightbulb, FaUser } from "react-icons/fa";
 
 function Navbar({ setPage, currentPage }) {
-  const activeStyle = "text-yellow-400";
-  const normalStyle = "text-gray-400";
+  const navItem = (icon, pageName) => (
+    <button
+      onClick={() => setPage(pageName)}
+      className={`text-xl ${
+        currentPage === pageName
+          ? "text-yellow-400"
+          : "text-white"
+      }`}
+    >
+      {icon}
+    </button>
+  );
 
   return (
-    <div className="fixed bottom-5 left-0 right-0 flex justify-center">
-      <div className="bg-black w-80 py-3 rounded-full flex justify-around items-center shadow-lg">
-
-        <button onClick={() => setPage("home")}>
-          <FaHome
-            size={20}
-            className={currentPage === "home" ? activeStyle : normalStyle}
-          />
-        </button>
-
-        <button onClick={() => setPage("history")}>
-          <FaFileAlt
-            size={20}
-            className={currentPage === "history" ? activeStyle : normalStyle}
-          />
-        </button>
-
-        <button onClick={() => setPage("tips")}>
-          <FaStar
-            size={20}
-            className={currentPage === "tips" ? activeStyle : normalStyle}
-          />
-        </button>
-
-        <button onClick={() => setPage("profile")}>
-          <FaUser
-            size={20}
-            className={currentPage === "profile" ? activeStyle : normalStyle}
-          />
-        </button>
-
+    <div className="fixed bottom-4 left-0 right-0 flex justify-center">
+      <div className="bg-black px-8 py-3 rounded-full flex gap-10 shadow-lg">
+        {navItem(<FaHome />, "home")}
+        {navItem(<FaHistory />, "history")}
+        {navItem(<FaLightbulb />, "tips")}
+        {navItem(<FaUser />, "profile")}
       </div>
     </div>
   );
